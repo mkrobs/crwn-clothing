ff73b191d5d66218e12f6a814515d5dd156904e8
mkrobs <dev@mikaelr.com>
2021-09-10 13:12:55 +0300

    added redux, app implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,40 +9,28 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-
-  }
-
   unsubscribefromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
     userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
+      setCurrentUser ({
             id: snapShot.id,
             ...snapShot.data()
-          }
-        });
-
-        console.log(this.state);
-        
+          });
       });
     }
 
-    else {this.setState({ currentUser: userAuth})};
+    setCurrentUser(userAuth);
      
     });
   }
@@ -55,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
     <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
       <Switch>
        <Route exact path='/' component={HomePage} />
        <Route path='/shop' component={ShopPage} />
@@ -67,5 +56,8 @@ class App extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -61,7 +61,7 @@ class SignIn extends React.Component {
                         label="password"
                         required />
                 <div className='buttons'>
-                    <CustomButton type="submit"> Sign In</CustomButton>
+                    <CustomButton type="button"> Sign In</CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                     Sign In with Google </CustomButton>
                 </div>
